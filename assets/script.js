7893e345f27ac7686e02911b2416257993dd1bcb
Floris <floris__2@hotmail.com>
2021-12-04 16:23:17 +0100

    Begin with EventListener

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 
 function macaron2() {
-    let image = document.getElementById("image-container");
-    image.src = "/assets/images/macaron_2.jpg";
+    document.getElementById("image-container").style.backgroundImage =
+          "url('/assets/images/macaron_2.jpg')";
 }
-
-
