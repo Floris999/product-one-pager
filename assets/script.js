1ce1fea75b5a2105931d6f02ee393d965f1c95e4
Floris <floris__2@hotmail.com>
2021-12-11 11:44:42 +0100

    event listenere fix on buttons

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,27 @@
 
+/* chage button 1 image*/
+function macaron1() {
+    document.getElementById("image-container").style.backgroundImage =
+          "url('/assets/images/macaron_1.jpg')";
+}
+
+/* chage button 2 image*/
+
 function macaron2() {
     document.getElementById("image-container").style.backgroundImage =
           "url('/assets/images/macaron_2.jpg')";
+}
+
+/* chage button 3 image*/
+
+function macaron3() {
+    document.getElementById("image-container").style.backgroundImage =
+          "url('/assets/images/macaron_3.jpg')";
+}
+
+/* chage button 4 image*/
+
+function macaron4() {
+    document.getElementById("image-container").style.backgroundImage =
+          "url('/assets/images/macaron_4.jpg')";
 }
